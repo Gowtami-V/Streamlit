fb152f1f932263b19b6283b73784e60bb4793b60
Gowtami-V <120001664+Gowtami-V@users.noreply.github.com>
2024-07-15 14:23:48 +0530

    Update todoapp.py

--- todoapp.py
+++ todoapp.py
@@ -34,20 +34,20 @@ def main():
 	elif choice == "Read":
 		st.subheader("View Items")
 		result = view_all_data()
-		st.write(result)
-		df = pd.DataFrame(result,columns=['Task','Status','Due Date'])
-		with st.expander("View All"):
-			st.dataframe(df)
-
-		with st.expander("Task Status"):
-			task_df = df['Status'].value_counts().to_frame()
-
-			# st.dataframe(task_df)
-			task_df = task_df.reset_index()
-			st.dataframe(task_df)
-
-			p1 = px.pie(task_df, names='index',values='Status')
-			st.plotly_chart(p1)
+		if result:
+			df = pd.DataFrame(result, columns=['Task', 'Status', 'Due Date'])
+			with st.expander("View All Data"):
+				st.dataframe(df)
+				
+			with st.expander("Task Status"):
+				task_df = df['Status'].value_counts().to_frame()
+				task_df = task_df.reset_index()
+				st.dataframe(task_df)
+				
+				p1 = px.pie(task_df, names='Status', values='count')
+				st.plotly_chart(p1)
+		else:
+			st.write("No tasks found.")
 			
 	elif choice == "Update":
 		st.subheader("Edit Items")
